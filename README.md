261e044abea73a23cecef195b8ad1e5cd0685be9
Su Li <hiiamsuli@gmail.com>
2019-10-15 22:05:56 -0700

    initial set up

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # spicy-wishboard
+
+semi-functional website made for friends in the use as a wishlist
+
+express & react & google app engine & maybe mongodb?
---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>spicy wishboard</h1>
+    </div>
+  );
+}
+
+export default App;
