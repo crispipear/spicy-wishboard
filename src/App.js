7ddf2926a9b797584927e9d2c5d9e4960b5117b7
crispipear <lisuy97@gmail.com>
2020-11-13 20:21:37 -0800

    set up react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import logo from './assets/logo.png';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo}/>
+    </div>
+  );
+}
+
+export default App;
